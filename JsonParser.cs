3a62590a66151af21d518a1e892ab1a9e0b4a9c1
Sten <62704405+ghostkiller967@users.noreply.github.com>
2022-05-20 22:11:17 +0200

    Add files via upload

---
+++ JsonParser.cs
@@ -0,0 +1,110 @@
+﻿namespace JsonLib
+{
+    public static class JsonParser
+    {
+        public static T FromJson<T>(this string json)
+        {
+            return typeof(T).IsGenericType ? (T)ParseValue(json.UnformatJson()).Item2 : (T)ParseObject(typeof(T), json.UnformatJson());
+        }
+        
+        public static string UnformatJson(this string json)
+        {
+            string unformatted = "";
+            for (int i = 0; i < json.Length; i++)
+            {
+                if (json[i] == '"')
+                {
+                    unformatted += json[i];
+                    for (int i2 = i + 1; i2 < json.Length; i2++)
+                    {
+                        unformatted += json[i2];
+                        if (json[i2] == '\\')
+                        {
+                            unformatted += json[i2 + 1];
+                            i2++;
+                        }
+                        else if (json[i2] == '"')
+                        {
+                            i = i2;
+                            break;
+                        }
+                    };
+                    continue;
+                }
+                if (char.IsWhiteSpace(json[i])) continue;
+                unformatted += json[i];
+            }
+            return unformatted;
+        }
+
+        public static List<string> Split(string json)
+        {
+            List<string> splitArray = new List<string>();
+            if (json.Length == 2) return splitArray;
+            int depth = 0;
+            string text = "";
+            for (int i = 1; i < json.Length - 1; i++)
+            {
+                if (json[i] == '{' || json[i] == '[') depth++;
+                else if (json[i] == '}' || json[i] == ']') depth--;
+                else if ((json[i] == ':' || json[i] == ',') && depth == 0)
+                {
+                    splitArray.Add(text);
+                    text = "";
+                    continue;
+                } else if (json[i] == '\"')
+                {
+                    text += json[i];
+                    for (int i2 = i + 1; i2 < json.Length; i2++)
+                    {
+                        text += json[i2];
+                        if (json[i2] == '\\')
+                        {
+                            text += json[i2 + 1];
+                            i2++;
+                        }
+                        else if (json[i2] == '"')
+                        {
+                            i = i2;
+                            break;
+                        }
+                    };
+                    continue;
+                }
+                text += json[i];
+            }
+            splitArray.Add(text);
+            return splitArray;
+        }
+
+        public static (Type, object) ParseValue(string value)
+        {
+            if (value.Length == 0 || value == "null") return (null, null); // Parse null value
+            else if (value[0] == '\"' && value[value.Length - 1] == '\"') return (typeof(string), value.Trim('"').Replace("\\", "")); // Parse string value
+            else if (value == "true" || value == "false") return (typeof(bool), value == "true" ? true : false); // Parse boolean value
+            else if (char.IsDigit(value[0]) || value[0] == '-') return value.Contains(".") ? (typeof(double), double.Parse(value)) : (typeof(int), int.Parse(value)); // Parse numbers
+            else if (value[0] == '[' && value[value.Length - 1] == ']') // Parse list
+            {
+                List<object> list = new List<object>();
+                foreach (string item in Split(value)) list.Add(ParseValue(item).Item2);
+                return (typeof(List<object>), list);
+            }
+            else if (value[0] == '{' && value[value.Length - 1] == '}') // Parse dictionary
+            {
+                List<string> items = Split(value);
+                Dictionary<string, object> dict = new Dictionary<string, object>();
+                for (int i = 0; i < items.Count; i += 2) dict.Add((string)ParseValue(items[i]).Item2, ParseValue(items[i + 1]).Item2);
+                return (typeof(Dictionary<string, object>), dict);
+            }
+            return (null, null);
+        }
+        
+        public static object ParseObject(Type type, string json)
+        {
+            object obj = Activator.CreateInstance(type);
+            foreach (KeyValuePair<string, object> item in (Dictionary<string, object>)ParseValue(json).Item2) 
+                type.GetProperty(item.Key).SetValue(obj, Convert.ChangeType(item.Value, type.GetProperty(item.Key).PropertyType));
+            return obj;
+        }
+    }
+}
---
+++ JsonWriter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace JsonLib
+{
+    public static class JsonWriter
+    {
+        public static string ToJson(this object obj)
+        {
+            return AddValue(obj, "");
+        }
+
+        private static string AddValue(object value, string json)
+        {
+            Type type = value.GetType();
+            if (type == typeof(string) || type == typeof(char))
+            {
+                json += "\"";
+                foreach (char c in value.ToString())
+                {
+                    if (c < 32 || c == '\"' || c == '\\')
+                    {
+                        if ("\"\\\n\r\t\b\f".Contains(c)) json += "\\" + "\"\\nrtbf"["\"\\\n\r\t\b\f".IndexOf(c)];
+                        else json += "\\u" + ((int)c).ToString("D4");
+                    }
+                    else json += c;
+                }
+                json += "\"";
+            }
+            else if (type == typeof(byte) || type == typeof(sbyte) || type == typeof(int) || type == typeof(uint) || type == typeof(short) || type == typeof(ushort) ||
+                     type == typeof(long) || type == typeof(ulong) || type == typeof(float) || type == typeof(double) || type == typeof(decimal)) json += value.ToString();
+            else if (type == typeof(bool)) json += (bool)value ? "true" : "false";
+            else if (type == typeof(DateTime) || type.IsEnum) json += $"\"{value}\"";
+            else if (value is IList)
+            {
+                json += "[";
+                foreach (object item in (IList)value)
+                {
+                    if (json[json.Length - 1] != '[') json += ",";
+                    json += AddValue(item, "");
+                }
+                json += "]";
+            }
+            else if (value is IDictionary)
+            {
+                if (type.GetGenericArguments()[0] != typeof(string)) return json + "{}";
+                json += "{";
+                foreach (DictionaryEntry item in (IDictionary)value)
+                {
+                    if (json[json.Length - 1] != '{') json += ",";
+                    json += $"{AddValue(item.Key, "")}:{AddValue(item.Value, "")}";
+                }
+                json += "}";
+            }
+            else if (value is null) json += "null";
+            else
+            {
+                json += "{";
+                foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(value))
+                {
+                    if (json[json.Length - 1] != '{') json += ",";
+                    json += $"\"{prop.Name}\":{AddValue(prop.GetValue(value), "")}";
+                }
+                json += "}";
+            }
+            return json;
+        }
+    }
+}
